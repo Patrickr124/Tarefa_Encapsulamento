93668753726802bef94ff3ba6dbd4aa6a534fad8
Patrick Rodrigues <107002662+Patrickr124@users.noreply.github.com>
2023-07-15 12:33:20 -0300

    Add files via upload

---
+++ Tarefa de Encapsulamento-Patrickr124/src/EntryPoint.java
@@ -0,0 +1,9 @@
+//Importe sua classe aqui
+
+class EntryPoint {
+    public static void main(String[] args) {
+        //Instancie sua classe aqui        
+    Pessoa pessoa=new Pessoa();
+    pessoa.digaOsSeusDados();
+    }
+}
---
+++ Tarefa de Encapsulamento-Patrickr124/src/Pessoa.java
@@ -0,0 +1,11 @@
+public class Pessoa {
+    private String nome="Patrick";
+    private int idade=26;
+    private int numerotelefone=982135912;
+
+public void digaOsSeusDados(){
+  System.out.println("Qual é o seu nome? " + nome +"\n"+"Qual é a sua idade? " + idade +" anos"+"\n"+ "E seu número de telefone? " + numerotelefone);  
+  
+}
+
+}
